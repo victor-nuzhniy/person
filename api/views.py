68fd67c8ec24adbd00632d1961f7c41d0039c89b
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-18 20:47:14 +0300

    dev created RegisterView

--- api/views.py
+++ api/views.py
@@ -1,1 +1,14 @@
 """Class and function views for api app."""
+
+from rest_framework.generics import CreateAPIView
+from rest_framework.schemas import AutoSchema
+from rest_framework.serializers import Serializer
+
+from api.serializers import UserSerializer
+
+
+class RegisterView(CreateAPIView):
+    """Class view for user registering."""
+
+    schema: AutoSchema = AutoSchema()
+    serializer_class: Serializer = UserSerializer
