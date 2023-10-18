ddfef2ecaa1e79c39880a5b129b4772615ba8f17
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-18 20:47:43 +0300

    dev created CustomUserAdmin in api.admin

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,35 @@
 """Admin site settings for api app."""
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    """Customized UserAdmin class with additional fields."""
+
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
