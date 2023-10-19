d82bef0f963e4b01877dde67ad03d5cf9a89d018
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-19 15:07:19 +0300

    tests added tests to test_views

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -1,12 +1,13 @@
 """Module for testing api app views."""
-from typing import Dict
+from typing import Dict, List, Tuple
 
 import pytest
 from django.test import Client
 from django.urls import reverse
 from faker import Faker
 
-from tests.api.factories import TeamFactory
+from api.models import User
+from tests.api.factories import TeamFactory, UserFactory
 
 
 @pytest.mark.django_db
@@ -31,3 +32,72 @@ class TestRegisterView:
         for key, value in data.items():
             if key != "password":
                 assert result[key] == value
+
+    def test_register_view_without_names(self, faker: Faker, client: Client) -> None:
+        """Test RegisterView."""
+        data: Dict = {
+            "email": faker.email(),
+            "password": faker.pystr(min_chars=1, max_chars=40),
+        }
+        url: str = reverse("sign_up")
+        response = client.post(url, data=data)
+        result: Dict = response.json()
+        assert response.status_code == 201
+        assert result.get("email") == data.get("email")
+
+    def test_register_view_invalid_email(self, faker: Faker, client: Client) -> None:
+        """Test RegisterView."""
+        data: Dict = {
+            "email": faker.pystr(min_chars=1, max_chars=10),
+            "password": faker.pystr(min_chars=1, max_chars=40),
+        }
+        url: str = reverse("sign_up")
+        response = client.post(url, data=data)
+        assert response.status_code == 400
+
+    def test_register_view_unique_email(self, faker: Faker, client: Client) -> None:
+        """Test RegisterView."""
+        user = UserFactory()
+        data: Dict = {
+            "email": user.email,
+            "password": faker.pystr(min_chars=1, max_chars=40),
+        }
+        url: str = reverse("sign_up")
+        response = client.post(url, data=data)
+        assert response.status_code == 400
+
+    def test_register_view_empty_password(self, faker: Faker, client: Client) -> None:
+        """Test RegisterView."""
+        data: Dict = {
+            "email": faker.email(),
+        }
+        url: str = reverse("sign_up")
+        response = client.post(url, data=data)
+        assert response.status_code == 400
+
+
+@pytest.mark.django_db
+class TestUserView:
+    """Class for testing UserView methods."""
+
+    pytestmark = pytest.mark.django_db
+
+    def test_user_get_method(
+        self,
+        faker: Faker,
+        client: Client,
+        get_authorized_admin_user_data: Tuple[User, Dict],
+    ) -> None:
+        """Test UserView get method."""
+        current_user, headers = get_authorized_admin_user_data
+        users: List[User] = UserFactory.create_batch(size=5)
+        user = faker.random_element(elements=users)
+        url: str = reverse("user", kwargs={"pk": user.id})
+        response = client.get(url, headers=headers)
+        result: Dict = response.json()
+        assert response.status_code == 200
+        for key, value in result.items():
+            if key == "team":
+                assert getattr(user, key).id == value
+            else:
+                assert getattr(user, key) == value
