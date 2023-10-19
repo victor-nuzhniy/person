8e9a938fb1bddc44b9081c1075379fca9e4f85cc
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-19 16:05:04 +0300

    tests added not admin user creation fixture

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -26,3 +26,22 @@ def get_authorized_admin_user_data(
         "Content-Type": "application/json",
     }
     return user, headers
+
+
+@pytest.fixture
+def get_authorized_user_data(
+    faker: Faker, django_user_model: User, client: Client
+) -> Tuple[User, Dict]:
+    """Create and get authorized user data for testing."""
+    user = django_user_model.objects.create_user(
+        email="test@gmail.com", password="password"
+    )
+    token_url: str = reverse("token")
+    token_data: Dict = {"email": user.email, "password": "password"}
+    response = client.post(token_url, data=token_data)
+    access_token = response.json().get("access")
+    headers: Dict = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    }
+    return user, headers
