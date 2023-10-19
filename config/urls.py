b27c82d3d557a524009e20e5041023e7ffd7b689
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-19 12:49:55 +0300

    dev changed schema_view settings in config urls

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from rest_framework import authentication
 from rest_framework.documentation import include_docs_urls
 
 schema_view = get_schema_view(
@@ -31,7 +31,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny],
+    authentication_classes=[authentication.BasicAuthentication],
 )
 
 
